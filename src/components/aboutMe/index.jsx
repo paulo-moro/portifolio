8244e062da88a14ea84f458ab22a65f4b567d928
Paulo Moro <paulo.morolol@gmail.com>
2023-05-24 17:15:23 -0300

    refactor: add more information of about me

--- src/components/aboutMe/index.jsx
+++ src/components/aboutMe/index.jsx
@@ -8,7 +8,10 @@ const AboutMe = ()=>{
                 Sou um mestre biotecnologista, que sempre teve curiosidade sobre programação, 
             depois de atuar na pandemia no apoio do diagnóstico do covid, acabei encarando uma
             nova oportunidade de mudar de área, sou naturalmente curioso e comunicativo, estou 
-            animado e ansioso para iniciar minha nova jornada.
+            animado e ansioso para iniciar minha nova jornada. Já tive experiencia profissional 
+            com desenvolvimento, com Vue.js 2.0  para frontend e node.js/express para backend.
+            Nessa experiência tive contato com TDD e Clean architeture, pretendo refatorar alguns
+            projetos pessoais adicionando esses conceitos.
             </p>
         </StyledSection>
     )
